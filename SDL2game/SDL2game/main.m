897c8f6448d5eef131fe0afd1e7899aeff88417d
Art User <macuser@mac-users-macbook-138.local>
2014-07-15 01:21:46 +0300

    First init

---
+++ SDL2game/SDL2game/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SDL2game
+//
+//  Created by Art User on 15.07.14.
+//  Copyright (c) 2014 Art User. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
